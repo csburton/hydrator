d74ce4ee06c9c1880502c249f37e3d8fdab82846
Chris Burton <chris.burton@revolutionbarsgroup.com>
2024-04-02 13:25:51 +0100

    Adding hydrate set

--- src/Extensions/PhpStan/HydratorReturnsCorrectType.php
+++ src/Extensions/PhpStan/HydratorReturnsCorrectType.php
@@ -8,6 +8,8 @@ use PHPStan\Analyser\Scope;
 use PhpParser\Node\Expr\MethodCall;
 use Pantono\Hydrator\Hydrator;
 use PHPStan\Type\Type;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\IntegerType;
 
 class HydratorReturnsCorrectType implements DynamicMethodReturnTypeExtension
 {
@@ -18,11 +20,15 @@ class HydratorReturnsCorrectType implements DynamicMethodReturnTypeExtension
 
     public function isMethodSupported(MethodReflection $methodReflection): bool
     {
-        return $methodReflection->getName() === 'hydrate';
+        return $methodReflection->getName() === 'hydrate' || $methodReflection->getName() === 'hydrateSet';
     }
 
     public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): ?Type
     {
+        if ($methodReflection->getName() === 'hydrateSet') {
+            $arg = $methodCall->getArgs()[0]->value;
+            return new ArrayType(new IntegerType(), $scope->getType($arg));
+        }
         $arg = $methodCall->getArgs()[0]->value;
         return $scope->getType($arg);
     }
